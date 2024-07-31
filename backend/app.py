e6b432a0b3ecf5f7f495ae8211c210eb95fccf8c
Paula <pbm4@hotmail.es>
2024-07-31 14:05:55 +0200

    prueba render.com 0.0.0.0

--- backend/app.py
+++ backend/app.py
@@ -1,21 +1,11 @@
 import uvicorn
-from threading import Thread
 from fastapi import FastAPI
 
 app = FastAPI()
 
-
-
+@app.get("/")
+async def read_root():
+    return {"Hello": "World"}
 
 if __name__ == "__main__":
-
-    @app.get("/")
-    async def read_root():
-        return {"Hello": "World"}
-
-    def run_server():
-        uvicorn.run(app, host="127.0.0.1", port=8000, log_level="info")
-
-    # Start the server in a separate thread
-    thread = Thread(target=run_server)
-    thread.start()
+    uvicorn.run(app, host="0.0.0.0", port=8000, log_level="info")
