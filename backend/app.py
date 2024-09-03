092de7d1c9e6af437f2e6c68546faa06cf38932c
Paula B. <72092576+PaulaBartolomeMora@users.noreply.github.com>
2024-09-03 13:51:41 +0200

    Prueba front+back con llm en groq (#5)
    * prueba 500
    
    * back logs
    
    * question to input
    
    * question to input
    
    * error pydantic str
    
    * modificado chain
    
    * modificado chain
    
    * chain.run
    
    * invoke ok en local
    
    * incompatibilidades versiones
    
    * front con env no ok en local, prueba en vercel

--- backend/app.py
+++ backend/app.py
@@ -1,26 +1,36 @@
 from dotenv import load_dotenv
-load_dotenv()  # Cargar variables de entorno desde el archivo .env
+import os
+import logging
 
 from fastapi import FastAPI, Form, Depends, HTTPException
 from fastapi.responses import RedirectResponse
 from fastapi.middleware.cors import CORSMiddleware
-
 import uvicorn
-import os
 
 from starlette.requests import Request
 from pydantic import BaseModel
 
+from langchain_groq import ChatGroq
+from langchain.chains import LLMChain
+from langchain.prompts import PromptTemplate
+
+"""from langchain_openai import OpenAI
+model = OpenAI(api_key=os.getenv('OPENAI_API_KEY'), temperature=0.7)
+"""
 
 #####import bbdd
 
+################################# INICIALIZACIÓN
 
+load_dotenv()  # Cargar variables de entorno desde el archivo .env
 
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
+llm = ChatGroq(model="llama3-70b-8192")
 app = FastAPI()
 
-# Configurar CORS
-app.add_middleware(
+app.add_middleware( # Configurar CORS
     CORSMiddleware,
     allow_origins=["*"],  # Permitir todas las URLs de origen. Puedes restringir esto a dominios específicos si lo prefieres.
     allow_credentials=True,
@@ -28,72 +38,10 @@ app.add_middleware(
     allow_headers=["*"],  # Permitir todos los encabezados HTTP.
 )
 
-
-
-"""
-users_db = {"user@example.com": {"password": "password123"}}
-
-class LoginForm(BaseModel):
-    email: str
-    password: str
-
-@app.post("/login")
-async def login_user(form_data: LoginForm):
-    email = form_data.email
-    password = form_data.password
-
-    if email not in users_db:
-        raise HTTPException(status_code=400, detail="Usuario no encontrado")
-    
-    if users_db[email]["password"] != password:
-        raise HTTPException(status_code=400, detail="Contraseña incorrecta")
-    
-    return RedirectResponse(url="/success", status_code=302)
-
-@app.get("/success")
-async def success():
-    return {"message": "Inicio de sesión exitoso"}
-"""
-
-
-
-
-
-
-"""from langchain_openai import OpenAI
-model = OpenAI(api_key=os.getenv('OPENAI_API_KEY'), temperature=0.7)
-"""
-
-from langchain_groq import ChatGroq
-llm = ChatGroq(model="llama3-70b-8192")
-
-from langchain.chains import LLMChain
-from langchain.prompts import PromptTemplate
-
-
-
 class InputModel(BaseModel):
     input: str
 
-@app.post("/ai")
-async def chat(input_model: InputModel):
-    if not input_model.input:
-        raise HTTPException(status_code=400, detail="No input provided")
-
-    template = "User: {question}\nBot:"
-    prompt = PromptTemplate(template=template, input_variables=['question'])
-
-    # Create the processing chain
-    chain = LLMChain(llm=llm, prompt=prompt)
-    
-    try:
-        # Get the response from the model
-        response = chain.run(question=input_model.input)
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
-    
-    return {"response": response.strip()}
-
+#################################
 
 @app.get("/")
 # """
@@ -103,6 +51,26 @@ async def chat(input_model: InputModel):
 def home():
     return {"response": "hola home"}
 
+@app.post("/ai")
+async def chat(input_model: InputModel):
+    if not input_model.input:
+        raise HTTPException(status_code=400, detail="No input provided")
+
+    template = "User: {input}\nBot:"
+    prompt = PromptTemplate(template=template, input_variables=['input'])
+
+    chain = LLMChain(llm=llm, prompt=prompt)
+    
+    try:
+        logger.info(f"Invoking LLMChain with input: {input_model.input}")
+        response = chain.invoke(input_model.input)  
+        response_text = response.get('text', 'No response text found')        
+        logger.info(f"Received response: {response_text}")
+    except Exception as e:
+        logger.error(f"Error during LLMChain invocation: {str(e)}")
+        raise HTTPException(status_code=500, detail=str(e))
+    
+    return {"response": response_text.strip()}
 
 
 if __name__ == "__main__":
